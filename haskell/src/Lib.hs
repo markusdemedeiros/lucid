843525044cabb953e1c6ccf348b3d4d983ee8009
markusde <markusde@student.ubc.ca>
2021-10-22 07:25:21 -0700

    update with new test files

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -8,6 +8,7 @@ import Control.Monad.Trans.Resource (runResourceT)
 import Dataflow
 import Processing (exampleFilter)
 import Reanimate (mkCircle, raster, reanimate)
+import Reanimate.Raster (mkImage)
 import Reanimate.Render (Format (RenderMp4), Raster (RasterAuto), render)
 import Sound.File.Sndfile (Sample)
 import Sources
@@ -22,6 +23,6 @@ runAudioPipelineTest :: IO ()
 runAudioPipelineTest =
   do
     fileconduit <- startFileStream "./data/test_data.flac" :: IO (SoundStream Double)
-    runConduitRes (exampleFilter fileconduit .| renderingConduit 0.2 (\s -> mkCircle 1.0)) >>= renderanim
+    runConduitRes (exampleFilter fileconduit .| renderingConduit 0.2 (\s -> mkImage 200 200 "./data/Haskell-Logo.svg")) >>= renderanim
   where
     renderanim animation = render animation "./data/output.mp4" RasterAuto RenderMp4 400 300 60 False
