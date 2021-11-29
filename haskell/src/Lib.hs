be31264c46068ecda74ef26053636308cb59d497
markusde <markusde@student.ubc.ca>
2021-11-28 21:27:05 -0800

    fourier methods

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -1,21 +1,48 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Lib where
 
+import Data.Complex 
 
-import qualified Data.Vector.Storable as SV
+import qualified Data.StorableVector as SV
 import Sound.File.Sndfile as SF
-import qualified Sound.File.Sndfile.Buffer.Vector as BV
+import qualified Sound.File.Sndfile.Buffer.StorableVector as BV
+import qualified Data.StorableVector.CArray as TCA
 
+import Math.FFT
+
+import Data.Array.CArray
+import qualified Data.Array.IArray as Arr
+
+-- For testing only
+import Graphics.EasyPlot
 
 __file_path :: FilePath
 __file_path = "./data/africa-toto.wav"
 
-
-__test :: IO (SV.Vector Double)
+__test :: IO () -- (CArray Int Double)
 __test = do
     (info, Just (x :: BV.Buffer Double)) <- SF.readFile __file_path
     putStrLn $ show info
-    return $ BV.fromBuffer x
+    -- return $ TCA.to $ BV.fromBuffer x
+    return () 
+
+
+
+-- __fft_test  :: IO (CArray Int (Complex Double))
+-- __fft_test = do
+--     __data <- __test
+--     return $ dftRC __data
+-- 
+-- 
+-- __graph_ft :: IO()
+-- __graph_ft = do
+--     fft_data <- __fft_test
+--     plot X11 $ Data2D [Title "FFT of a sin wave"] [] (zip [1..] $ SV.unpack $ TCA.from fft_data)
+--     return ()
+
+
+
+
 
 
 
